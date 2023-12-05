832afdc48dadc3c1be79455a25d96b02acf0297b
Chaejun Lee <chaejunlee@knu.ac.kr>
2023-12-04 23:45:14 -0800

    day2: part 1 - 31:35

---
+++ day2/index.ts
@@ -0,0 +1,42 @@
+import { readFileSync } from "fs";
+import { cwd } from "process";
+
+const currentDirectory = cwd();
+const filename = currentDirectory + "/input.txt";
+
+const rawData = readFileSync(filename, "utf8");
+
+const data = rawData.split("\n") as string[];
+
+const RED_CUBES = 12;
+const GREEN_CUBES = 13;
+const BLUE_CUBES = 14;
+
+const CUBES = {
+  red: 12,
+  green: 13,
+  blue: 14,
+};
+
+type Color = keyof typeof CUBES;
+
+const answer = data.reduce((acc, cur, index) => {
+  const splitData = cur.split(/[:;]/);
+  const [_, ...games] = [...splitData];
+
+  for (let game of games) {
+    const balls = game.split(",");
+
+    for (let ball of balls) {
+      const [num, color] = ball.trim().split(" ");
+      if (color && num && CUBES[color as Color] < parseInt(num)) {
+        console.log("WRONG:", index, num, CUBES[color as Color]);
+        return acc;
+      }
+    }
+  }
+
+  return acc + index + 1;
+}, 0);
+
+console.log(answer);
