ad797176953cef90e24639c08fd56bd7fd1a40e0
Chaejun Lee <chaejunlee@knu.ac.kr>
2023-12-16 17:02:55 -0800

    day8: part 2 - 1:35:16

--- day8/index.ts
+++ day8/index.ts
@@ -12,6 +12,7 @@ const instructions = data[0]?.trim().split("")!;
 
 const directions = data[1]?.split("\n").reduce((acc, cur) => {
   const [from, to] = cur.split(" = ") as [string, string];
+  console.log(from, to);
   const [left, right] = to?.replace("(", "").replace(")", "").split(", ") as [
     string,
     string
@@ -27,18 +28,93 @@ const directions = data[1]?.split("\n").reduce((acc, cur) => {
   return acc;
 }, {} as Record<string, { left: string; right: string }>)!;
 
-let steps = 0;
-let pos = "AAA";
+// let steps = 0;
+// let pos = "AAA";
 
-while (pos != "ZZZ") {
-  const dir = instructions[steps % instructions.length];
+// while (pos != "ZZZ") {
+//   const dir = instructions[steps % instructions.length];
 
-  if (dir === "L") {
-    pos = directions[pos]?.left!;
-  } else {
-    pos = directions[pos]?.right!;
+//   if (dir === "L") {
+//     pos = directions[pos]?.left!;
+//   } else {
+//     pos = directions[pos]?.right!;
+//   }
+//   steps++;
+// }
+
+// console.log(steps);
+
+let part2 = 0;
+
+let positions = Object.keys(directions).filter((v) => v[2] === "A");
+
+// console.log(positions);
+
+const loops = positions.reduce((acc, cur) => {
+  let steps = 0;
+  let pos = cur;
+  let done = false;
+
+  while (!done) {
+    const dir = instructions[steps % instructions.length];
+
+    if (dir === "L") {
+      pos = directions[pos]?.left!;
+    } else {
+      pos = directions[pos]?.right!;
+    }
+
+    if (pos[2] === "Z") {
+      const acccur = acc[cur];
+      if (acccur) {
+        const acccurpos = acccur[pos];
+        if (acccurpos) {
+          const repeated = acccurpos.some(
+            (v: number) =>
+              v % instructions.length === steps % instructions.length
+          );
+          acccurpos.push(steps);
+          if (repeated) {
+            done = true;
+            return acc;
+          }
+        } else {
+          acccur[pos]?.push(steps);
+        }
+      } else {
+        acc[cur] = {
+          [pos]: [steps],
+        };
+      }
+    }
+    steps++;
   }
-  steps++;
+  return acc;
+}, {} as Record<string, Record<string, number[]>>);
+
+const flatArr = Object.values(loops)
+  .map((v) => {
+    return Object.values(v).flat() as [number, number];
+  })
+  .map((v: [number, number]) => ({
+    start: v[0],
+    jump: v[1] - v[0],
+  }))
+  .reduce((acc, cur) => {
+    return lcm(acc, cur.jump);
+  }, 1);
+
+function gcd(a: number, b: number) {
+  if (b) {
+    return gcd(b, a % b);
+  }
+  return Math.abs(a);
 }
 
-console.log(steps);
+function lcm(a: number, b: number) {
+  let g = gcd(a, b);
+
+  return (a * b) / g;
+}
+
+console.log(flatArr);
