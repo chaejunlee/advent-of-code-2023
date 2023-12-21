556915948b0052e175e3ec8f9fcbd647e2505288
Chaejun Lee <chaejunlee@knu.ac.kr>
2023-12-21 01:45:40 -0800

    day10: part 2 - 1:35:01

--- day10/index.ts
+++ day10/index.ts
@@ -45,7 +45,6 @@ let visited = Array.from({ length: data.length }, () => new Array(data[0]!.lengt
 
 while (outerQ.length) {
   steps++;
-  console.log(outerQ);
   let q = [...outerQ];
   outerQ = [];
   while (q.length) {
@@ -68,4 +67,70 @@ while (outerQ.length) {
   }
 }
 
-console.log(steps);
+let insideTiles = 0;
+
+const verticalWall = ["|", "F", "J", "L", "7"];
+const verticalFrom: Record<string, string[]> = {
+  "F": ["7"],
+  "L": ["J"],
+  "|": ["|", "L", "F"]
+}
+const horizontalWall = ["-", "F", "J", "L", "7"];
+const horizontalFrom: Record<string, string[]> = {
+  "F": ["L"],
+  "7": ["J"],
+  "-": ["-", "F", "7"]
+}
+
+let horSwipe: [number,number][] = [];
+let verSwipe: [number,number][] = [];
+
+for (let i = 0; i < data.length; i++) {
+  let inside = false;
+  let from = "|";
+  for (let j = 0; j < data[0]!.length; j++) {
+    const currChar = data[i]![j]!;
+    const visit = visited[i]![j]!;
+
+    if (!visit && inside) {
+      horSwipe.push([i, j]);
+    }
+
+    if (visit && verticalWall.includes(currChar)) {
+      if (verticalFrom[from]?.includes(currChar)) {
+        inside = !inside;
+      }
+      from = ["7", "J"].includes(currChar) ? "|" : currChar;
+    }
+  }
+}
+
+for (let j = 0; j < data[0]!.length; j++) {
+  let inside = false;
+  let from = "-";
+  for (let i = 0; i < data.length; i++) {
+    const currChar = data[i]![j]!;
+    const visit = visited[i]![j]!;
+
+    if (!visit && inside) {
+      verSwipe.push([i, j]);
+    }
+
+    if (visit && horizontalWall.includes(currChar)) {
+      if (horizontalFrom[from]?.includes(currChar)) {
+        inside = !inside;
+      }
+      from = ["J", "L"].includes(currChar) ? "-" : currChar;
+    }
+  }
+}
+
+console.log(horSwipe, verSwipe);
+
+console.log(horSwipe.filter(v => {
+  for (const ver of verSwipe) {
+    if (ver[0] == v[0] && ver[1] === v[1])
+      return true;
+  }
+  return false;
+}).length);
